6012f5aecf3300068a8f421a847908c8c585f0c7
MrCloudExplorer <110226727+Vasanthabalaji01@users.noreply.github.com>
2024-04-24 13:23:07 +0530

    Update cloudcli.sh

--- cloudcli.sh
+++ cloudcli.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Banner Display
 echo "
   __  __ _      _    ___ _             _    ___ _    ___ 
  |  \/  (_)_ _ (_)  / __| |___ _  _ __| |  / __| |  |_ _|
- | |\/| | | ' \| | | (__| / _ \ || / _` | | (__| |__ | | 
+ | |\/| | | ' \| | | (__| / _ \ || / _\` | | (__| |__ | | 
  |_|  |_|_|_||_|_|  \___|_\___/\_,_\__,_|  \___|____|___|
-
-Developed by Mr.CloudExplorer | Version 0.2 "
+"
+echo "Developed by Mr.CloudExplorer | Version 0.1"
 
 # Main Menu
 while true; do
